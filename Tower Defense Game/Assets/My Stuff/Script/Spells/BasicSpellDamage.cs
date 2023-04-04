7f5304aba69162936608bb501aff4fc217c5bce6
Jacob Palin <jacobcpalin@gmail.com>
2023-04-04 15:03:23 -0600

    enemies can die now

--- Tower Defense Game/Assets/My Stuff/Script/Spells/BasicSpellDamage.cs
+++ Tower Defense Game/Assets/My Stuff/Script/Spells/BasicSpellDamage.cs
@@ -7,6 +7,7 @@ public class BasicSpellDamage : MonoBehaviour
     public int damage;
     private void OnTriggerEnter(Collider other)
     {
+        //Debug.Log("collision detected");
         if(other.TryGetComponent(out TestNavmeshEnemy health))
         {
             //Debug.Log("they should take damage");
